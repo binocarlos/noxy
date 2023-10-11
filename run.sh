fb797d515105ea7ca2e217965a917d4df2010510
Luke Marsden <me@lukemarsden.net>
2023-10-11 15:21:13 +0100

    Support keycloak

--- run.sh
+++ run.sh
@@ -38,6 +38,9 @@ function get_proxy_config() {
       proxy_connect_timeout   180;
       proxy_send_timeout      180;
       proxy_read_timeout      180;
+      proxy_buffer_size       128k;
+      proxy_buffers           4 256k;
+      proxy_busy_buffers_size 256k;
 end-of-proxy-config
 }
 
